5ab7837fabd464736508e95c63a415d11eb1d36e
Simon Schneegans <code@simonschneegans.de>
2023-10-07 19:31:13 +0200

    :sparkles: Format with prettier

--- index.js
+++ index.js
@@ -4,23 +4,23 @@
 //                         Copyright: (c) 2020 Simon Schneegans                         //
 //////////////////////////////////////////////////////////////////////////////////////////
 
-import core from '@actions/core';
-import { makeBadge } from 'badge-maker';
+import core from "@actions/core";
+import { makeBadge } from "badge-maker";
 
-const gistUrl = new URL(core.getInput('gistID'), core.getInput('host'));
+const gistUrl = new URL(core.getInput("gistID"), core.getInput("host"));
 
 // This uses the method above to update a gist with the given data. The user agent is
 // required as defined in https://developer.github.com/v3/#user-agent-required
 async function updateGist(body) {
   const headers = new Headers([
-    ['Content-Type', 'application/json'],
-    ['Content-Length', body.length],
-    ['User-Agent', 'Schneegans'],
-    ['Authorization', `token ${core.getInput('auth')}`],
+    ["Content-Type", "application/json"],
+    ["Content-Length", body.length],
+    ["User-Agent", "Schneegans"],
+    ["Authorization", `token ${core.getInput("auth")}`],
   ]);
 
   const response = await fetch(gistUrl, {
-    method: 'POST',
+    method: "POST",
     headers,
     body,
   });
@@ -34,7 +34,7 @@ async function updateGist(body) {
     return;
   }
 
-  console.log('Success!');
+  console.log("Success!");
 }
 
 // We wrap the entire action in a try / catch block so we can set it to "failed" if
@@ -44,27 +44,27 @@ try {
   // and message attributes are always required. All others are optional and added to the
   // content object only if they are given to the action.
   let data = {
-    label: core.getInput('label'),
-    message: core.getInput('message'),
+    label: core.getInput("label"),
+    message: core.getInput("message"),
   };
 
-  const filename = core.getInput('filename');
-  const isSvgFile = filename.endsWith('.svg');
+  const filename = core.getInput("filename");
+  const isSvgFile = filename.endsWith(".svg");
 
   if (!isSvgFile) {
     data.schemaVersion = 1;
   }
 
   // Compute the message color based on the given inputs.
-  const color = core.getInput('color');
-  const valColorRange = core.getInput('valColorRange');
-  const minColorRange = core.getInput('minColorRange');
-  const maxColorRange = core.getInput('maxColorRange');
-  const invertColorRange = core.getInput('invertColorRange');
-  const colorRangeSaturation = core.getInput('colorRangeSaturation');
-  const colorRangeLightness = core.getInput('colorRangeLightness');
+  const color = core.getInput("color");
+  const valColorRange = core.getInput("valColorRange");
+  const minColorRange = core.getInput("minColorRange");
+  const maxColorRange = core.getInput("maxColorRange");
+  const invertColorRange = core.getInput("invertColorRange");
+  const colorRangeSaturation = core.getInput("colorRangeSaturation");
+  const colorRangeLightness = core.getInput("colorRangeLightness");
 
-  if (minColorRange != '' && maxColorRange != '' && valColorRange != '') {
+  if (minColorRange != "" && maxColorRange != "" && valColorRange != "") {
     const max = parseFloat(maxColorRange);
     const min = parseFloat(minColorRange);
     let val = parseFloat(valColorRange);
@@ -73,75 +73,75 @@ try {
     if (val > max) val = max;
 
     let hue = 0;
-    if (invertColorRange == '') {
+    if (invertColorRange == "") {
       hue = Math.floor(((val - min) / (max - min)) * 120);
     } else {
       hue = Math.floor(((max - val) / (max - min)) * 120);
     }
 
     let sat = 100;
-    if (colorRangeSaturation != '') {
+    if (colorRangeSaturation != "") {
       sat = parseFloat(colorRangeSaturation);
     }
 
     let lig = 40;
-    if (colorRangeLightness != '') {
+    if (colorRangeLightness != "") {
       lig = parseFloat(colorRangeLightness);
     }
 
-    data.color = 'hsl(' + hue + ', ' + sat + '%, ' + lig + '%)';
-  } else if (color != '') {
+    data.color = "hsl(" + hue + ", " + sat + "%, " + lig + "%)";
+  } else if (color != "") {
     data.color = color;
   }
 
   // Get all optional attributes and add them to the content object if given.
-  const labelColor = core.getInput('labelColor');
-  const isError = core.getInput('isError');
-  const namedLogo = core.getInput('namedLogo');
-  const logoSvg = core.getInput('logoSvg');
-  const logoColor = core.getInput('logoColor');
-  const logoWidth = core.getInput('logoWidth');
-  const logoPosition = core.getInput('logoPosition');
-  const style = core.getInput('style');
-  const cacheSeconds = core.getInput('cacheSeconds');
+  const labelColor = core.getInput("labelColor");
+  const isError = core.getInput("isError");
+  const namedLogo = core.getInput("namedLogo");
+  const logoSvg = core.getInput("logoSvg");
+  const logoColor = core.getInput("logoColor");
+  const logoWidth = core.getInput("logoWidth");
+  const logoPosition = core.getInput("logoPosition");
+  const style = core.getInput("style");
+  const cacheSeconds = core.getInput("cacheSeconds");
 
-  if (labelColor != '') {
+  if (labelColor != "") {
     data.labelColor = labelColor;
   }
 
-  if (!isSvgFile && isError != '') {
+  if (!isSvgFile && isError != "") {
     data.isError = isError;
   }
 
-  if (!isSvgFile && namedLogo != '') {
+  if (!isSvgFile && namedLogo != "") {
     data.namedLogo = namedLogo;
   }
 
-  if (!isSvgFile && logoSvg != '') {
+  if (!isSvgFile && logoSvg != "") {
     data.logoSvg = logoSvg;
   }
 
-  if (!isSvgFile && logoColor != '') {
+  if (!isSvgFile && logoColor != "") {
     data.logoColor = logoColor;
   }
 
-  if (!isSvgFile && logoWidth != '') {
+  if (!isSvgFile && logoWidth != "") {
     data.logoWidth = parseInt(logoWidth);
   }
 
-  if (!isSvgFile && logoPosition != '') {
+  if (!isSvgFile && logoPosition != "") {
     data.logoPosition = logoPosition;
   }
 
-  if (style != '') {
+  if (style != "") {
     data.style = style;
   }
 
-  if (!isSvgFile && cacheSeconds != '') {
+  if (!isSvgFile && cacheSeconds != "") {
     data.cacheSeconds = parseInt(cacheSeconds);
   }
 
-  let content = '';
+  let content = "";
 
   if (isSvgFile) {
     content = makeBadge(data);
@@ -155,16 +155,16 @@ try {
 
   // If "forceUpdate" is set to true, we can simply update the gist. If not, we have to
   // get the gist data and compare it to the new value before.
-  if (core.getBooleanInput('forceUpdate')) {
+  if (core.getBooleanInput("forceUpdate")) {
     updateGist(body);
   } else {
     // Get the old gist.
     fetch(gistUrl, {
-      method: 'GET',
+      method: "GET",
       headers: new Headers([
-        ['Content-Type', 'application/json'],
-        ['User-Agent', 'Schneegans'],
-        ['Authorization', `token ${core.getInput('auth')}`],
+        ["Content-Type", "application/json"],
+        ["User-Agent", "Schneegans"],
+        ["Authorization", `token ${core.getInput("auth")}`],
       ]),
     })
       .then((response) => {
